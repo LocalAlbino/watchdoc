bb17adaf1d36c1a82c6d6b8c47a168ddb99ef8f0
Rudy Hartwig <rudyhartwig25@outlook.com>
2025-11-22 16:48:24 -0500

    fix: program now properly handles when no config is given

--- src/watchdoc/config.py
+++ src/watchdoc/config.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 from watchdoc.cli import verbose
 
@@ -6,7 +7,12 @@ from watchdoc.cli import verbose
 @verbose
 def load_config():
     print("loading configuration file.")
-    with open("watchdoc.json", "r") as f:
-        config = json.load(f)
-        print("finished loading configuration file.")
-        return config
+    try:
+        with open("../../watchdoc.json", "r") as f:
+            config = json.load(f)
+            print("finished loading configuration file.")
+            return config
+    except FileNotFoundError:
+        print("configuration file not found.")
+        print("see https://github.com/LocalAlbino/watchdoc for info on how to set up watchdoc.")
+        sys.exit(1)
